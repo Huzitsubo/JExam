e5fd807a44762945bdb214dc3fa9ebd03b96a9d3
user <user@192.168.0.110>
2016-05-20 17:14:17 +0900

    コミット4

--- JExam10_6/src/com/psol/Main.java
+++ JExam10_6/src/com/psol/Main.java
@@ -79,7 +79,7 @@ public class Main {
 		System.out.println("********************************************");
 		System.out.printf("\n\n");
 		System.out.println("////////////////////////////////////////////");
-		
+		customer.register();
 		System.out.println("////////////////////////////////////////////");
 	}
 
